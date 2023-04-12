67598ae420f2e9be53dbbdaac24718bdb25ccb59
FuyaKoshiro <s.castle28546@gmail.com>
2023-04-12 15:59:50 -0700

    updated database

--- common/models/create_model_users.py
+++ common/models/create_model_users.py
@@ -3,12 +3,14 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
+cursor.execute("drop table users")
+
 cursor.execute("""
 CREATE TABLE users (
 user_id INTEGER,
-video_code CHAR(100),
-phrase_id CHAR(200),
-date DATESTAMP)
+user_name CHAR,
+user_email CHAR(200),
+user_password CHAR)
 """)
 
 conn.commit()
