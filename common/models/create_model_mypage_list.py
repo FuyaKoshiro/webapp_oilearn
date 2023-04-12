993172ae33b8df6d173de13a2447765deb3a4870
FuyaKoshiro <s.castle28546@gmail.com>
2023-04-12 10:29:22 -0700

    updated

--- common/models/create_model_mypage_list.py
+++ common/models/create_model_mypage_list.py
@@ -7,18 +7,18 @@ c.execute("""
 CREATE TABLE mypage (
 mypage_id INTEGER PRIMARY KEY,
 user_id INTEGER,
-video_id CHAR(50),
+video_code CHAR(50),
 phrases CHAR(50),
 meaning CHAR(50),
 date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY(user_id) REFERENCES users(user_id),
-FOREIGN KEY(video_id) REFERENCES video(video_id))
+FOREIGN KEY(video_code) REFERENCES videos(video_code))
 """)
 
 conn.commit()
 
 c.execute("""
-INSERT INTO mypage (mypage_id, user_id, video_id, phrases, meaning, date) VALUES (?, ?, ?, ?, ?, ?)
+INSERT INTO mypage (mypage_id, user_id, video_code, phrases, meaning, date) VALUES (?, ?, ?, ?, ?, ?)
 """, (1, 1, 'test_video_id', 'test_phrase', "test_meaning", "test_time"))
 
 conn.commit()
--- common/models/create_model_videos.py
+++ common/models/create_model_videos.py
@@ -3,21 +3,26 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
+c.execute("drop table videos")
+conn.commit()
+
 c.execute("""
 CREATE TABLE videos (
 video_id INTEGER PRIMARY KEY,
+channel_url CHAR(500),
 title CHAR(500),
 url CHAR(2083),
 thumbnail CHAR(2083),
 phrase CHAR(50),
-meaning CHAR(50))
+meaning CHAR(50),
+video_code CHAR(50))
 """)
 
 conn.commit()
 
 c.execute("""
-INSERT INTO videos (video_id, title, url, thumbnail, phrase, meaning) VALUES (?, ?, ?, ?, ?, ?)
-""", (1, "test_title", 'test_url', 'test_thumbnail', "testphrase", "test_meaning"))
+INSERT INTO videos (video_id, channel_url, title, url, thumbnail, phrase, meaning, video_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+""", (1, "test_channel_url", "test_title", 'test_url', 'test_thumbnail', "testphrase", "test_meaning", "test_video_code"))
 
 conn.commit()
 conn.close()
