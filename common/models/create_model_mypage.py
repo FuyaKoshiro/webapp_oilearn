ad66c238568bafe83666ef6f99e217988bee7f2a
FuyaKoshiro <s.castle28546@gmail.com>
2023-04-12 16:13:40 -0700

    fixed a bit

--- common/models/create_model_mypage.py
+++ common/models/create_model_mypage.py
@@ -6,10 +6,10 @@ cursor = conn.cursor()
 user_ids = range(100)
 mypage_names = [str(user_ids[i])+"_mypage" for i in user_ids]
 
+
 for mypage_name in mypage_names:
     cursor.execute(f"""
     CREATE TABLE `{mypage_name}` (
-    user_id INTEGER,
     video_code CHAR,
     phrase_id CHAR,
     date DATESTAMP)
